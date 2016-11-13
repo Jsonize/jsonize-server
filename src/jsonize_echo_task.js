d379f6aaf7c4efbbca923f08d2e31b93e52f25d9
Oliver Friedmann <oliver.friedmann@gmail.com>
2016-11-13 07:53:26 -0500

    More Test Tasks

--- src/jsonize_echo_task.js
+++ src/jsonize_echo_task.js
@@ -15,3 +15,48 @@ Scoped.define("jsonize:JsonizeEchoTask", [
 	
 	return Cls;
 });
+
+
+
+Scoped.define("jsonize:JsonizeEchoFailTask", [
+    "jsonize:AbstractJsonizeTask",
+    "jsonize:JsonizeTaskRegistry",
+    "betajs:Promise"
+], function (Class, TaskRegistry, Promise, scoped) {
+	var Cls = Class.extend({scoped: scoped}, {
+
+		_run: function (payload) {
+			return Promise.error(payload);
+		}
+			
+	});
+	
+	TaskRegistry.register("echofail", Cls);
+	
+	return Cls;
+});
+
+
+Scoped.define("jsonize:JsonizeEchoTimeoutTask", [
+    "jsonize:AbstractJsonizeTask",
+    "jsonize:JsonizeTaskRegistry",
+    "betajs:Promise"
+], function (Class, TaskRegistry, Promise, scoped) {
+	var Cls = Class.extend({scoped: scoped}, {
+
+		_run: function (payload) {
+			var promise = Promise.create();
+			setTimeout(function () {
+				promise.asyncSuccess(payload);
+			}, payload.delay);
+			return promise;
+		}
+			
+	});
+	
+	TaskRegistry.register("echotimeout", Cls);
+	
+	return Cls;
+});
+
+
